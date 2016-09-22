fixed dnase normalization; label generation

src/datagen.py
@@ -117,28 +117,13 @@ class DataGenerator:
             celltype_names = list(labels.columns[3:])
 
             for celltype in celltype_names:
-                pdb.set_trace()
-                celltype_labels = np.array(labels.celltype)
+                celltype_labels = np.array(labels[celltype])
                 unbound_indices = np.where(celltype_labels == 'U')
                 y[unbound_indices, tf_lookup[transcription_factor], celltype_lookup[celltype]] = 0
                 ambiguous_indices = np.where(celltype_labels == 'A')
                 y[ambiguous_indices, tf_lookup[transcription_factor], celltype_lookup[celltype]] = 0
-                bound_indices = np.where(celltype_labels == 'C')
+                bound_indices = np.where(celltype_labels == 'B')
                 y[bound_indices, tf_lookup[transcription_factor], celltype_lookup[celltype]] = 1
-            '''
-            with gzip.open(path) as fin:
-                celltype_names = fin.readline().split()[3:]
-                for l_idx, line in enumerate(fin):
-                    tokens = line.split()
-                    bound_states = tokens[3:]
-
-                    for i in range(len(bound_states)):
-                        if bound_states[i] == 'B':
-                            y[l_idx, tf_lookup[transcription_factor], celltype_lookup[celltype_names[i]]] = 1
-                        else:
-                            y[l_idx, tf_lookup[transcription_factor], celltype_lookup[celltype_names[i]]] = 0
-            '''
-            pdb.set_trace()
 
         np.save(os.path.join(self.save_dir, 'y.npy'), y)
 
@@ -165,6 +150,7 @@ class DataGenerator:
         return lookup
 
     def get_sequece_from_ids(self, ids, segment):
+        ids.sort()
         return
 
 

src/preprocess.py
@@ -202,21 +202,16 @@ def preprocess_chipseq(num_jobs, bin_size):
 
 
 def parallel_normalize_dnaseq(ifpath, ofpath):
-    print ifpath
-    dnase_features = pd.read_csv(ifpath, delimiter=" ", dtype=np.float16)
+    dnase_features = pd.read_csv(ifpath, delimiter=" ", dtype=np.float32)
     # downsample
-    dnase_array = np.array(dnase_features, dtype=np.float16)
-    dnase_downsampled = []
-    for row in dnase_array:
-        dnase_downsampled.append(np.hstack((row[0:3], [np.mean(row[3+6*i:9+6*i]) for i in range(10)])))
-    dnase_downsampled = np.array(dnase_downsampled, dtype=np.float16)
+    dnase_features = np.array(dnase_features, dtype=np.float32)
+    dnase_features = np.hstack((dnase_features[:, :3], dnase_features[:, 3::6]))
 
-    dnase_downsampled = pd.DataFrame(dnase_downsampled)
-    dnase_downsampled = (dnase_downsampled - dnase_downsampled.mean()) / dnase_downsampled.std()
-    dnase_norm = np.array(dnase_downsampled, dtype=np.float16)
-    dnase_norm[np.isnan(dnase_norm) | np.isinf(dnase_norm)] = 0
+    dnase_norm = StandardScaler().fit_transform(dnase_features)
+    dnase_norm[np.isnan(dnase_norm)] = 0
+    dnase_norm[np.isinf(dnase_norm)] = 0
 
-    np.save(ofpath+'.npy', dnase_norm)
+    np.save(ofpath+'.npy', dnase_norm.astype(np.float16))
 
 
 def normalize_dnaseq(num_jobs):
@@ -238,6 +233,7 @@ def normalize_dnaseq(num_jobs):
     for i in range(0, len(processes), num_jobs):
         map(lambda x: x.start(), processes[i:i + num_jobs])
         map(lambda x: x.join(), processes[i:i + num_jobs])
+
 
 if __name__ == '__main__':
 